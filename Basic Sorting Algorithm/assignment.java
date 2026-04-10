7175aae22d00976adbc5d8c50edcfe121847ef3f
noking1212 <prabhsamrathsingh1212@gmail.com>
2026-04-10 23:17:18 +0530

    Add files via upload

---
+++ Basic Sorting Algorithm/assignment.java
@@ -0,0 +1,28 @@
+public class assignment {
+    public static void main(String args[]){
+        int list[] = {5,4,1,3,2};
+        int n = list.length;
+        //!bubble sort
+        // for(int i =0;i<n-1;i++){
+        //     for(int j =0;j<n-i-1;j++){
+        //         int swap =0; 
+        //         if(list[j]>list[j+1]){
+        //             swap = list[j];
+        //             list[j] = list[j+1];
+        //             list[j+1] = swap;
+        //         }
+        //     }
+        
+        // }
+
+        //!selection sort
+        for(int i=0;i<n-1;i++){
+            int minpos =0;
+            for(int j=i;j<n;j++)
+        }
+
+        for(int i=0;i<n;i++){
+            System.out.println(list[i]+" ");
+        }
+    }
+}
